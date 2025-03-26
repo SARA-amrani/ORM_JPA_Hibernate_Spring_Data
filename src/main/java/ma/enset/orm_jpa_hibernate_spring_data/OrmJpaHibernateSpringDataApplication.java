c65fb870265a7d54ab904aa8077c2b3eef5f8c32
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-03-26 17:51:18 +0000

    Créeation de l'entité JPA Patient

--- src/main/java/ma/enset/orm_jpa_hibernate_spring_data/OrmJpaHibernateSpringDataApplication.java
+++ src/main/java/ma/enset/orm_jpa_hibernate_spring_data/OrmJpaHibernateSpringDataApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrmJpaHibernateSpringDataApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrmJpaHibernateSpringDataApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrmJpaHibernateSpringDataApplication.class, args);
+    }
 
 }
--- src/test/java/ma/enset/orm_jpa_hibernate_spring_data/OrmJpaHibernateSpringDataApplicationTests.java
+++ src/test/java/ma/enset/orm_jpa_hibernate_spring_data/OrmJpaHibernateSpringDataApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OrmJpaHibernateSpringDataApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
